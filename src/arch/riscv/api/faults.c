c7d6b95926aedfb2ed8963c551d395ebbdca9db8
Hesham Almatary <hesham.almatary@data61.csiro.au>
2016-08-31 14:41:42 +1000

    RISC-V: copy handleFaultReply implementation from x86

--- src/arch/riscv/api/faults.c
+++ src/arch/riscv/api/faults.c
@@ -17,5 +17,76 @@
 bool_t
 handleFaultReply(tcb_t *receiver, tcb_t *sender)
 {
+    seL4_MessageInfo_t tag;
+    word_t         label;
+    fault_t        fault;
+    unsigned int   length;
+
+    /* These lookups are moved inward from doReplyTransfer */
+    tag = messageInfoFromWord(getRegister(sender, msgInfoRegister));
+    label = seL4_MessageInfo_get_label(tag);
+    length = seL4_MessageInfo_get_length(tag);
+    fault = receiver->tcbFault;
+
+    switch (fault_get_faultType(fault)) {
+    case fault_cap_fault:
+        return true;
+
+    case fault_vm_fault:
+        return true;
+
+    case fault_unknown_syscall: {
+        word_t i;
+        register_t   r;
+        word_t       v;
+        word_t*      sendBuf;
+
+        sendBuf = lookupIPCBuffer(false, sender);
+
+        /* Assumes n_syscallMessage > n_msgRegisters */
+        for (i = 0; i < length && i < n_msgRegisters; i++) {
+            r = syscallMessage[i];
+            v = getRegister(sender, msgRegisters[i]);
+            setRegister(receiver, r, sanitiseRegister(r, v));
+        }
+
+        if (sendBuf) {
+            for (; i < length && i < n_syscallMessage; i++) {
+                r = syscallMessage[i];
+                v = sendBuf[i + 1];
+                setRegister(receiver, r, sanitiseRegister(r, v));
+            }
+        }
+    }
+    return (label == 0);
+
+    case fault_user_exception: {
+        word_t i;
+        register_t   r;
+        word_t       v;
+        word_t*      sendBuf;
+
+        sendBuf = lookupIPCBuffer(false, sender);
+
+        /* Assumes n_exceptionMessage > n_msgRegisters */
+        for (i = 0; i < length && i < n_msgRegisters; i++) {
+            r = exceptionMessage[i];
+            v = getRegister(sender, msgRegisters[i]);
+            setRegister(receiver, r, sanitiseRegister(r, v));
+        }
+
+        if (sendBuf) {
+            for (; i < length && i < n_exceptionMessage; i++) {
+                r = exceptionMessage[i];
+                v = sendBuf[i + 1];
+                setRegister(receiver, r, sanitiseRegister(r, v));
+            }
+        }
+    }
+    return (label == 0);
+
+    default:
+        fail("Invalid fault");
+    }
     return false;
 }
