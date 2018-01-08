8dd66b9a5eda15c0e5e0374529032ffdef9856fa
Hesham Almatary <hesham.almatary@unsw.edu.au>
2018-01-08 13:07:23 +1100

    [Review]: RISC-V - Round2 - arch-specific headers and data structures

---
+++ src/arch/riscv/model/statedata.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+
+#include <util.h>
+#include <api/types.h>
+#include <arch/types.h>
+#include <arch/model/statedata.h>
+#include <arch/object/structures.h>
+#include <linker.h>
+#include <plat/machine/hardware.h>
+
+/* The top level asid mapping table */
+asid_pool_t *riscvKSASIDTable[BIT(asidHighBits)];
+
+pte_t l1pt[BIT(PT_INDEX_BITS)] ALIGN(BIT(seL4_PageTableBits));
+pte_t l2pt[BIT(PT_INDEX_BITS)] ALIGN(BIT(seL4_PageTableBits));
+pte_t l3pt[BIT(PT_INDEX_BITS)] ALIGN(BIT(seL4_PageTableBits));
