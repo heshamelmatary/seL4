f6d3b9525bb8780043723b8b13184c54dd33f227
Hesham Almatary <hesham.almatary@unsw.edu.au>
2018-01-09 14:14:38 +1100

    [Review] RISC-V include/arch/riscv/ mode 64-bit headers

---
+++ include/arch/riscv/arch/64/mode/types.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+
+#ifndef __ARCH_MODE_TYPES_H
+#define __ARCH_MODE_TYPES_H
+
+#include <config.h>
+#include <assert.h>
+#include <stdint.h>
+
+compile_assert(long_is_64bits, sizeof(unsigned long) == 8)
+
+#define wordRadix 6
+
+#endif
